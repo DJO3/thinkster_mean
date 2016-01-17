463f4c14edd6417e4ea1b7be1385223bd4f32b0a
DJO3 <dev@pytools.org>
2016-01-17 17:53:41 -0500

    Angular Services

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var app = angular.module('flapperNews', [])
 
+// {Title: Post, Link: url}
+app.factory('posts', [function (){
+  var obj = {
+    posts: []
+  }
+  return obj
+}])
+
 app.controller('MainCtrl', [
   '$scope',
-  function ($scope) {
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ]
+  'posts',
+  function ($scope, posts) {
+    $scope.posts = posts.posts
 
     $scope.addPost = function () {
       if (!$scope.title || $scope.title === '') { return }
