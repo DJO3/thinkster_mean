eedd37a2f3ddf7131ebfdb3c8eba044466ff1dd3
DJO3 <dev@pytools.org>
2016-01-21 19:56:46 -0500

    Opening REST Routes

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -4,17 +4,18 @@ var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
-var mongoose = require('mongoose')
 
+// Database & Models
+var mongoose = require('mongoose')
+require('./models/Posts')
+require('./models/Comments')
+mongoose.connect('mongodb://localhost/news')
+
+// Routes
 var routes = require('./routes/index')
 var users = require('./routes/users')
 
 var app = express()
-
-// Database setup
-require('./models/posts.js')
-require('./models/comments.js')
-mongoose.connect('mongodb://localhost/news')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
