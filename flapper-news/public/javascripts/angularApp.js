9e77dd3d856311990d0ca337c5cdd1b13e3a9ca2
DJO3 <dev@pytools.org>
2016-01-23 12:04:19 -0500

    Wiring Everything Up

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -9,12 +9,22 @@ app.config([
       .state('home', {
         url: '/home',
         templateUrl: '/home.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['posts', function (posts) {
+            return posts.getAll()
+          }]
+        }
       })
       .state('posts', {
         url: '/posts/{id}',
         templateUrl: '/posts.html',
-        controller: 'PostsCtrl'
+        controller: 'PostsCtrl',
+        resolve: {
+          post: ['$stateParams', 'posts', function ($stateParams, posts) {
+            return posts.get($stateParams.id)
+          }]
+        }
       })
 
     $urlRouterProvider.otherwise('home')
@@ -23,10 +33,48 @@ app.config([
 
 // Factories
 // {Title: Post, Link: url}
-app.factory('posts', [function () {
+app.factory('posts', ['$http', function ($http) {
   var o = {
     posts: []
   }
+
+  o.getAll = function () {
+    return $http.get('/posts')
+      .success(function (data) {
+        angular.copy(data, o.posts)
+      })
+  }
+
+  o.create = function (post) {
+    return $http.post('/posts', post).success(function (data) {
+      o.posts.push(data)
+    })
+  }
+
+  o.upvote = function (post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function () {
+        post.upvotes += 1
+      })
+  }
+
+  o.get = function (id) {
+    return $http.get('/posts/' + id).then(function (res) {
+      return res.data
+    })
+  }
+
+  o.addComment = function (id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment)
+  }
+
+  o.upvoteComment = function (post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
+      .success(function () {
+        comment.upvotes += 1
+      })
+  }
+
   return o
 }])
 
@@ -38,44 +86,44 @@ app.controller('MainCtrl', [
     $scope.posts = posts.posts
 
     $scope.addPost = function () {
-      if (!$scope.title || $scope.title === '') { return }
-      $scope.posts.push({
+      if (!$scope.title || $scope.title === '') {
+        return
+      }
+      posts.create({
         title: $scope.title,
-        link: $scope.link,
-        upvotes: 0,
-        comments: [
-          {author: 'Joe', body: 'Cool post!', upvotes: 0},
-          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-        ]
+        link: $scope.link
       })
       $scope.title = ''
       $scope.link = ''
     }
 
     $scope.incrementUpvotes = function (post) {
-      post.upvotes += 1
+      posts.upvote(post)
     }
   }
 ])
 
 app.controller('PostsCtrl', [
   '$scope',
-  '$stateParams',
   'posts',
-  function ($scope, $stateParams, posts) {
-    $scope.post = posts.posts[$stateParams.id]
+  'post',
+  function ($scope, posts, post) {
+    $scope.post = post
 
     $scope.addComment = function () {
-      if ($scope.body === '') { return }
-      $scope.post.comments.push({
+      if ($scope.body === '') {
+        return
+      }
+      posts.addComment(post._id, {
         body: $scope.body,
-        author: 'user',
-        upvotes: 0
+        author: 'user'
+      }).success(function (comment) {
+        $scope.post.comments.push(comment)
       })
       $scope.body = ''
     }
-    $scope.incrementUpvotes = function (post) {
-      post.upvotes += 1
+    $scope.incrementUpvotes = function (comment) {
+      posts.upvoteComment(post, comment)
     }
   }
 ])
